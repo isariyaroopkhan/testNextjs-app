649d846f1d04a7ce2c953283a4eb715261b9a310
isariyaroopkhan <primmyisariya@gmail.com>
2024-05-30 12:01:39 +0700

    first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function PageNotfound({}: Props) {
+  return (
+    <div>PageNotfound</div>
+  )
+}
---
+++ src/app/report/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+    params : any
+}
+
+export default function Report({params}: Props) {
+  return (
+    <div>Report id: {params.id}</div>
+  )
+}
